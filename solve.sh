47a9ba68a85764f5892a351bf3656a426282b92a
Rubmary Rojas <rubmaryrojas@gmail.com>
2019-06-13 00:00:57 -0400

    Improve script

--- solve.sh
+++ solve.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-echo "Making dirs"
-mkdir -p graphics/$1
-mkdir -p regret/$1
-mkdir -p results/$1
+function solve_game() {
+	echo "Making dirs"
+	mkdir -p graphics/$1
+	mkdir -p regret/$1
+	mkdir -p results/$1
+
+	echo "Running cfr"
+	./cfr $1
+	echo "Running gebr"
+	./gebr $1
+	echo "Creating graphics"
+	python src/graph.py $1
+}
 
 echo "Compiling programs"
 make cfr
 make gebr
-
-
-echo "Running cfr"
-./cfr $1
-echo "Running gebr"
-./gebr $1
-echo "Creating graphics"
-pytho src/graphs.py $1
-
+solve_game "KuhnPoker"
 echo "Cleaning"
 make clean
